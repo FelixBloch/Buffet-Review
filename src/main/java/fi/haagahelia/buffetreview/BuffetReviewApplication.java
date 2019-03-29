010e8d272a5ecacb27904724fccd2221bbf6d085
FelixBloch <felix.bloch@myy.haaga-helia.fi>
2019-03-29 12:21:10 +0200

    Adding the rest of the buffet-review application

---
+++ src/main/java/fi/haagahelia/buffetreview/BuffetReviewApplication.java
@@ -0,0 +1,22 @@
+package fi.haagahelia.buffetreview;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BuffetReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetReviewApplication.class, args);
+	}
+	
+	/*@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+			
+		};
+	}*/
+
+}
---
+++ src/main/java/fi/haagahelia/buffetreview/domain/User.java
@@ -0,0 +1,72 @@
+package fi.haagahelia.buffetreview.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, updatable = false)
+	private long id;
+	
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+	@Column(name = "password", nullable = false)
+	private String passwordHash;
+	@Column(name = "role", nullable = false)
+	private String role;
+	@Column(name = "firstName", nullable = false)
+	private String firstName;
+	@Column(name = "lastName", nullable = false)
+	private String lastName;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
